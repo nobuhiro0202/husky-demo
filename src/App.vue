65a71d370a8b0bcb2e77c68f6037ab4f07799591
nobuhiro0202 <Nobu.s1322@gmail.com>
2024-01-22 03:59:14 +0900

    コミット成功が期待値

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue';
 import TheWelcome from './components/TheWelcome.vue';
+const a = () => 1;
+console.log(a());
 </script>
 
 <template>
