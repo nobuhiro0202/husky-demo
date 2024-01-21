1c92b957293574ace329df477e10f4639c3b54ea
nobuhiro0202 <Nobu.s1322@gmail.com>
2024-01-22 02:59:59 +0900

    jrstjw454

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue';
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You coul do it!" />
+      <HelloWorld msg="You could do it!" />
     </div>
   </header>
 
